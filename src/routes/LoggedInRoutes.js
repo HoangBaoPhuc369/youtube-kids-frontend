4d39a437e12e606e3ba7380880e5fc810c7ed230
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-12-08 08:10:53 +0700

    Update protect route

--- src/routes/LoggedInRoutes.js
+++ src/routes/LoggedInRoutes.js
@@ -4,5 +4,6 @@ import Login from "../pages/login";
 
 export default function LoggedInRoutes() {
   const { user } = useSelector((state) => ({ ...state.auth }));
+  const { childrenActive } = useSelector((state) => state.children);
   return user ? <Outlet /> : <Login />; //<Navigate to="/login" />
 }
--- src/routes/NotLoggedInRoutes.js
+++ src/routes/NotLoggedInRoutes.js
@@ -3,6 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function NotLoggedInRoutes() {
   const { user } = useSelector((state) => ({ ...state.auth }));
-
-  return user ? <Navigate to="/" /> : <Outlet />;
+  const { childrenActive } = useSelector((state) => state.children);
+  return user && childrenActive ? <Navigate to="/" /> : <Outlet />;
 }
