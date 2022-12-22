cb08fe0b12ca96064d7f9c792bfba8e45347e629
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-12-22 15:58:44 +0700

    Update tracking activity socket

--- src/component/modals/ChooseProfilePictureSettings.js
+++ src/component/modals/ChooseProfilePictureSettings.js
@@ -29,7 +29,7 @@ export default function ChooseProfilePictureSettings({
     <Modal
       show={show}
       onHide={onHide}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
